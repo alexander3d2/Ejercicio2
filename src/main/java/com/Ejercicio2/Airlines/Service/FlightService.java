ea59b35c5ebde9eb13f4c745aef1d1a4ac03166d
alexander3d2 <147887436+alexander3d2@users.noreply.github.com>
2024-11-16 12:49:10 -0500

    Ejercicio2

---
+++ src/main/java/com/Ejercicio2/Airlines/Service/FlightService.java
@@ -0,0 +1,24 @@
+package com.Ejercicio2.Airlines.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class FlightService {
+
+    @Value("${aviation.api.key}")
+    private String apiKey;
+
+    private static final String API_URL = "https://developers.amadeus.com/";  // Cambia esta URL según la API que utilices
+
+    public String getFlightSchedule(String airlineCode) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = UriComponentsBuilder.fromHttpUrl(API_URL)
+                .queryParam("api_key", apiKey)
+                .queryParam("airline_code", airlineCode)
+                .toUriString();
+        return restTemplate.getForObject(url, String.class);
+    }
+}
---
+++ src/test/java/com/Ejercicio2/Airlines/AirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejercicio2.Airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
